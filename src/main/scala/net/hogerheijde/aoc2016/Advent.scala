28ba0a417c87508506b6514e558dba4c0592fe34
Matthias Hogerheijde <matthias@hogerheijde.net>
2016-12-12 21:50:35 +0100

    Refactor day1 input into file

--- src/main/scala/net/hogerheijde/aoc2016/Advent.scala
+++ src/main/scala/net/hogerheijde/aoc2016/Advent.scala
@@ -15,13 +15,7 @@ object Advent {
 
 
   def runDay1(): Unit = {
-    val instructions = "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, " +
-      "L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, " +
-      "L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, " +
-      "R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, " +
-      "L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, " +
-      "R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, " +
-      "R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
+    val instructions = Util.readFile("net/hogerheijde/aoc2016/days/day1.input")
     val distance1 = Day1.build(instructions).run()
     println(s"Day 1 - pt1: $distance1 (expect 279)")
     val distance2 = Day1.build(instructions).runPart2()

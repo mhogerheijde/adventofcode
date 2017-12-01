c4734af81009f044d1275ccc303aefbec0f29ce6
Matthias Hogerheijde <matthias.hogerheijde@ebpi.nl>
2017-12-01 17:16:58 +0100

    Day 1; part 2

--- 2017/src/main/scala/net/hogerheijde/aoc2017/day1/Day1.scala
+++ 2017/src/main/scala/net/hogerheijde/aoc2017/day1/Day1.scala
@@ -4,20 +4,32 @@ import scala.io.Source
 
 object Day1 {
 
+
+
   def main(args: Array[String]): Unit = {
-    val input = Source.fromResource("net/hogerheijde/aoc2017/day1.input").mkString
+    val input = Source.fromResource("net/hogerheijde/aoc2017/day1.input").mkString.trim
 
     val result1 = part1(input)
     println(s"Day 1; part 1: $result1")
+    val result2 = part2(input)
+    println(s"Day 1; part 2: $result2")
 
 
   }
 
-  def part1(i: String): Int = {
-    val input = i.trim
+  def part1(input: String): Int = {
     (input + input.charAt(0)).toSeq.sliding(2).foldLeft(0) { case (total, chars) =>
       if (chars.distinct.length == 1) { total + Integer.parseInt(chars.head.toString, 10) } else { total }
     }
+  }
+
+  def part2(str: String): Int = {
+    val rotated = str.substring(str.length/2) + str.substring(0, str.length/2)
+    str.zip(rotated).foldLeft(0) { case (total, (char1, char2)) =>
+      if (char1 == char2) { total + Integer.parseInt(char1.toString, 10) } else { total }
+    }
+
+
   }
 
 
--- 2017/src/test/scala/net/hogerheijde/aoc2017/day1/Day1Test.scala
+++ 2017/src/test/scala/net/hogerheijde/aoc2017/day1/Day1Test.scala
@@ -7,9 +7,18 @@ class Day1Test extends FlatSpec with Matchers {
 
   "Day 1; part 1" should "calclulate value" in {
     Day1.part1("1122") should be(3)
-    Day1.part1("1111\n") should be(4)
+    Day1.part1("1111") should be(4)
     Day1.part1("1234") should be(0)
     Day1.part1("91212129") should be(9)
   }
+
+  "Day 1; part 2" should "calclulate value" in {
+    Day1.part2("1212") should be(6 )
+    Day1.part2("1221") should be(0 )
+    Day1.part2("123425") should be(4)
+    Day1.part2("123123") should be(12)
+    Day1.part2("12131415") should be(4)
+  }
+
 
 }

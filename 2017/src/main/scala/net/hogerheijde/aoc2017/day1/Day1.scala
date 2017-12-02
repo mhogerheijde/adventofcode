c0a182727f6c78acb725444f8ddac7258398748b
Matthias Hogerheijde <matthias@hogerheijde.net>
2017-12-02 10:24:44 +0100

    2017: Enable scalastyle

--- 2017/src/main/scala/net/hogerheijde/aoc2017/day1/Day1.scala
+++ 2017/src/main/scala/net/hogerheijde/aoc2017/day1/Day1.scala
@@ -8,16 +8,18 @@ object Day1 extends Day[String, Int, Int]{
   override def name: String = "Day 1"
   override def parse: String => String = identity
 
+  private val BaseTen = 10
+
   def part1(input: String): Int = {
     (input + input.charAt(0)).toSeq.sliding(2).foldLeft(0) { case (total, chars) =>
-      if (chars.distinct.length == 1) { total + Integer.parseInt(chars.head.toString, 10) } else { total }
+      if (chars.distinct.length == 1) { total + Integer.parseInt(chars.head.toString, BaseTen) } else { total }
     }
   }
 
   def part2(str: String): Int = {
     val rotated = str.substring(str.length/2) + str.substring(0, str.length/2)
     str.zip(rotated).foldLeft(0) { case (total, (char1, char2)) =>
-      if (char1 == char2) { total + Integer.parseInt(char1.toString, 10) } else { total }
+      if (char1 == char2) { total + Integer.parseInt(char1.toString, BaseTen) } else { total }
     }
   }
 

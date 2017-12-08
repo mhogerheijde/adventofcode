d128c12e9734d1e16bd2234916962677ea750088
Matthias Hogerheijde <matthias.hogerheijde@ebpi.nl>
2017-12-08 13:39:14 +0100

    Run all days from Application

--- 2017/src/main/scala/net/hogerheijde/aoc2017/Application.scala
+++ 2017/src/main/scala/net/hogerheijde/aoc2017/Application.scala
@@ -1,11 +1,25 @@
 package net.hogerheijde.aoc2017
 
 import net.hogerheijde.aoc2017.day1.Day1
+import net.hogerheijde.aoc2017.day2.Day2
+import net.hogerheijde.aoc2017.day3.Day3
+import net.hogerheijde.aoc2017.day4.Day4
+import net.hogerheijde.aoc2017.day5.Day5
+import net.hogerheijde.aoc2017.day6.Day6
+import net.hogerheijde.aoc2017.day7.Day7
+import net.hogerheijde.aoc2017.day8.Day8
 
 object Application {
   def main(args: Array[String]): Unit = {
     printHeader()
     Day1.run()
+    Day2.run()
+    Day3.run()
+    Day4.run()
+    Day5.run()
+    Day6.run()
+    Day7.run()
+    Day8.run()
   }
 
 

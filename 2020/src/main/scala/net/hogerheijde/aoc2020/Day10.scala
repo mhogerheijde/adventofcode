a438878d9de810e61719030334297dabae8b8187
Matthias Hogerheijde <matthias@hogerheijde.net>
2020-12-12 23:26:02 +0100

    2020 - Day 10 part 2

--- 2020/src/main/scala/net/hogerheijde/aoc2020/Day10.scala
+++ 2020/src/main/scala/net/hogerheijde/aoc2020/Day10.scala
@@ -16,9 +16,27 @@ object Day10 extends Day[Long, Long]{
       .sliding(2)
       .map { case head +: tail => tail.foldLeft(head) { (acc, next) => next - acc }}
       .toSeq
+
+    println(differences)
     differences.count(_ == 1) * differences.count(_ == 3)
   }
 
+  val pathCountByConsequtive = Map(
+    0 -> 1L,
+    1 -> 1L,
+    2 -> 2L,
+    3 -> 4L,
+    4 -> 7L,
+  )
 
-  override def part2(input: Day10.Model): Long = ???
+  override def part2(input: Day10.Model): Long = input
+    .sorted
+    .sliding(2)
+    .map { case head +: tail => tail.foldLeft(head) { (acc, next) => next - acc }}
+    .foldLeft(Seq(0)) {
+      case (acc, 1) => acc.init :+ (acc.last + 1)
+      case (acc, _) => acc :+ 0
+    }
+    .map(pathCountByConsequtive)
+    .product
 }
--- 2020/src/test/scala/net/hogerheijde/aoc2020/Day10Test.scala
+++ 2020/src/test/scala/net/hogerheijde/aoc2020/Day10Test.scala
@@ -9,8 +9,8 @@ class Day10Test extends AnyWordSpec with Matchers {
   val example2 = Seq(0, 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3, 52)
     .map(_.toLong)
 
-  "Day 9" should {
-    "solve example" in {
+  "Day 10" should {
+    "solve example part 1" in {
 
 
       Day10.part1(example1) should be (35)
@@ -19,6 +19,11 @@ class Day10Test extends AnyWordSpec with Matchers {
 
       Day10.part1(example2) should be (220)
 
+    }
+
+    "solve example part 2" in {
+      Day10.part2(example1) should be (8)
+      Day10.part2(example2) should be (19208)
     }
   }
 }

bf9f4432b989415125799a1de521b1528e27414f
Matthias Hogerheijde <matthias@hogerheijde.net>
2022-12-06 14:31:42 +0100

    2022 - Day 6 - Rewrite using indexWhere

--- 2022/src/main/scala/net/hogerheijde/aoc2022/Day6.scala
+++ 2022/src/main/scala/net/hogerheijde/aoc2022/Day6.scala
@@ -3,15 +3,12 @@ package net.hogerheijde.aoc2022
 import net.hogerheijde.aoc.util.Day
 
 object Day6 extends Day[Int, Int] {
-  override type Model = Seq[Char]
-  override def parse(input: String): Model = input.toSeq
-
+  override type Model = String
   
+  override def parse(input: String): Model = input
+
   def findMarker(input: Model, windowSize: Int): Int = {
-    val marker = input.sliding(windowSize).zipWithIndex.find { case (quadrupel, _) =>
-      quadrupel.toSet.size == windowSize
-    }
-    marker.get._2 + windowSize
+    input.toSeq.sliding(windowSize).indexWhere(_.toSet.size == windowSize) + windowSize
   } 
   
   override def part1(input: Model): Int = findMarker(input, 4)
--- 2022/src/test/scala/net/hogerheijde/aoc2022/Day6Test.scala
+++ 2022/src/test/scala/net/hogerheijde/aoc2022/Day6Test.scala
@@ -8,18 +8,18 @@ class Day6Test extends AnyWordSpec with Matchers {
   
   "Day 6" should {
     "solve part 1" in {
-      Day6.part1("bvwbjplbgvbhsrlpgdmjqwftvncz".toSeq) should be(5)
-      Day6.part1("nppdvjthqldpwncqszvftbrmjlhg".toSeq) should be(6)
-      Day6.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".toSeq) should be(10)
-      Day6.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".toSeq) should be(11)
+      Day6.part1("bvwbjplbgvbhsrlpgdmjqwftvncz") should be(5)
+      Day6.part1("nppdvjthqldpwncqszvftbrmjlhg") should be(6)
+      Day6.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") should be(10)
+      Day6.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") should be(11)
 
     }
     "solve part 2" in {
-      Day6.part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb".toSeq) should be(19)
-      Day6.part2("bvwbjplbgvbhsrlpgdmjqwftvncz".toSeq) should be(23)
-      Day6.part2("nppdvjthqldpwncqszvftbrmjlhg".toSeq) should be(23)
-      Day6.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".toSeq) should be(29)
-      Day6.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".toSeq) should be(26)
+      Day6.part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") should be(19)
+      Day6.part2("bvwbjplbgvbhsrlpgdmjqwftvncz") should be(23)
+      Day6.part2("nppdvjthqldpwncqszvftbrmjlhg") should be(23)
+      Day6.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") should be(29)
+      Day6.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") should be(26)
     }
   }
 }

237dd2afea7b7f8251bff03c3de4066e3073dd71
Matthias Hogerheijde <matthias@hogerheijde.net>
2025-12-16 09:25:06 +0100

    2025: Day 6, part 1 & 2

--- 2025/src/main/scala/net/hogerheijde/aoc2025/Day6.scala
+++ 2025/src/main/scala/net/hogerheijde/aoc2025/Day6.scala
@@ -10,7 +10,7 @@ import net.hogerheijde.aoc.util.Parser
 
 import scala.collection.immutable
 
-object Day6 extends Day[Long, Int]:
+object Day6 extends Day[Long, Long]:
 
   sealed trait Symbol
   object Symbol:
@@ -19,22 +19,86 @@ object Day6 extends Day[Long, Int]:
       case Add
       case Multiply
 
-  type Model = Grid[Symbol]
+  type Model = String
 
-  override def parse(input: String): Model = Parser.parse(grid(_))(input).get
+  override def parse(input: String): Model = input
 
   override def part1(input: Model): Long =
-    input.columns.view.mapValues(col =>
-      col.collectFirst { case x: Symbol.Operation => x } match {
-        case Some(Symbol.Operation.Add)      => col.collect { case Symbol.Number(v) => v }.sum.toLong
-        case Some(Symbol.Operation.Multiply) => col.collect { case Symbol.Number(v) => v }.foldLeft(1L)(_ * _)
-        case None                            => throw RuntimeException(s"There should always be an operation in a column, found none in $col")
-      }
-    )
-      .values
-      .foldLeft(0L)(_ + _)
+    Parser.parse(grid(_))(input)
+      .get
+      .columns
+      .view.mapValues(col =>
+        col.collectFirst { case x: Symbol.Operation => x } match {
+          case Some(Symbol.Operation.Add)      => col.collect { case Symbol.Number(v) => v }.sum.toLong
+          case Some(Symbol.Operation.Multiply) => col.collect { case Symbol.Number(v) => v }.foldLeft(1L)(_ * _)
+          case None                            => throw RuntimeException(s"There should always be an operation in a column, found none in $col")
+        }
+      )
+        .values
+        .safeSum
 
-  override def part2(input: Model): Int = 0
+  override def part2(input: Model): Long =
+    ColumnReader(input)
+      .extractExpressions()
+      .map(_.solve)
+      .safeSum
+
+
+  class ColumnReader(input: String):
+    private var position = 0;
+    private val lines: IndexedSeq[String] = input.linesIterator.toIndexedSeq
+    private val columns = lines.init
+    private val operators = lines.last
+
+    def nextColumn(): Option[(Long, Option[Symbol.Operation])] =
+      if position >= lines(0).length then
+        None
+      else
+        val num = columns.map(_.charAt(position)).foldLeft("")(_ + _).trim
+        val operator = operators.charAt(position)
+        position += 1
+
+        (num, operator) match
+          case ("", ' ') =>
+            None
+          case (n, o) =>
+            val number = n.toLong
+            val operation = o match {
+              case '+' => Some(Symbol.Operation.Add)
+              case '*' => Some(Symbol.Operation.Multiply)
+              case ' ' => None
+            }
+            Some((number, operation))
+
+
+    def nextExpression(partial: Option[Expression] = None): Option[Expression] =
+      nextColumn() match
+        case None => partial
+        case Some(long, None) =>
+          val p: Option[Expression] = partial.map(x => x :+ long)
+          nextExpression(p)
+        case Some(long, Some(operation)) =>
+          nextExpression(Some(Expression(long, operation)))
+
+    def extractExpressions(): Seq[Expression] =
+      Iterator
+        .continually(nextExpression())
+        .takeWhile(_.isDefined)
+        .flatten
+        .toSeq
+
+  case class Expression(
+    values: Seq[Long],
+    operation: Symbol.Operation,
+  ):
+    def `:+`(l: Long): Expression = copy(values = values :+ l)
+    def solve: Long = operation match
+      case Symbol.Operation.Add      => values.safeSum
+      case Symbol.Operation.Multiply => values.safeProduct
+
+  object Expression:
+    def apply(l: Long, o: Symbol.Operation): Expression = Expression(Seq(l), o)
+
 
   def number[$: P]: P[Symbol.Number] = P(int).map(Symbol.Number.apply)
   def add[$: P]: P[Symbol.Operation] = P("+").map(_ => Symbol.Operation.Add)
@@ -43,7 +107,7 @@ object Day6 extends Day[Long, Int]:
   def symbol[$: P]: P[Symbol] = P(number | operation)
 
   def line[$: P]: P[Seq[Symbol]] = P((" ".rep ~ symbol).rep(min = 1))
-  def grid[$: P]: P[Grid[Symbol]] = P((line ~ "\n".?).rep(1)).map { x =>
+  def grid[$: P]: P[Grid[Symbol]] = P((line ~ (" ".rep ~ "\n")).rep(1)).map { x =>
     Grid(
       x.zipWithIndex.flatMap((symbols, line) =>
         symbols.zipWithIndex.map((symbol, column) =>
@@ -52,6 +116,10 @@ object Day6 extends Day[Long, Int]:
       )
     )
   }
+
+  extension (l: Iterable[Long])
+    def safeSum: Long = l.foldLeft(0L)(_ + _)
+    def safeProduct: Long = l.foldLeft(1L)(_ * _)
 
   extension [T] (g: Grid[T])
     def columns: Map[Int, immutable.Iterable[T]] =
--- 2025/src/test/scala/net/hogerheijde/aoc2025/Day6Test.scala
+++ 2025/src/test/scala/net/hogerheijde/aoc2025/Day6Test.scala
@@ -3,20 +3,20 @@ package net.hogerheijde.aoc2025
 import net.hogerheijde.aoc.common.model.Coordinate
 import net.hogerheijde.aoc.common.model.Grid
 import net.hogerheijde.aoc.util.Parser
+import net.hogerheijde.aoc2025.Day6.ColumnReader
+import net.hogerheijde.aoc2025.Day6.Expression
 import net.hogerheijde.aoc2025.Day6.Symbol
-import net.hogerheijde.aoc2025.Day6.Symbol.Operation.Add
-import net.hogerheijde.aoc2025.Day6.Symbol.Operation.Multiply
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Day6Test extends AnyWordSpec with Matchers {
 
   val exampleInput: String =
-    """123 328  51 64
-      | 45 64  387 23
-      |  6 98  215 314
-      |*   +   *   +
-      |""".stripMargin
+      "123 328  51 64 \n" +
+      " 45 64  387 23 \n" +
+      "  6 98  215 314\n" +
+      "*   +   *   +  \n"
+
 
   val exampleModel: Grid[Symbol] = Grid(
     Seq(
@@ -42,6 +42,66 @@ class Day6Test extends AnyWordSpec with Matchers {
     )
   )
 
+  "ColumnReader" should {
+    "extract expression" in {
+      val reader = ColumnReader(exampleInput)
+
+      reader.nextExpression() should be (
+        Some(
+          Expression(
+            values = Seq(
+              1L,
+              24L,
+              356L,
+            ),
+            operation = Day6.Symbol.Operation.Multiply
+          )
+        )
+      )
+
+      reader.nextExpression() should be (
+        Some(
+          Expression(
+            values = Seq(
+              369L,
+              248L,
+              8L,
+            ),
+            operation = Day6.Symbol.Operation.Add
+          )
+        )
+      )
+
+      reader.nextExpression() should be(
+        Some(
+          Expression(
+            values = Seq(
+              32L,
+              581L,
+              175L,
+            ),
+            operation = Day6.Symbol.Operation.Multiply
+          )
+        )
+      )
+
+      reader.nextExpression() should be(
+        Some(
+          Expression(
+            values = Seq(
+              623L,
+              431L,
+              4L,
+            ),
+            operation = Day6.Symbol.Operation.Add
+          )
+        )
+      )
+
+      reader.nextExpression() should be(None)
+    }
+  }
+
   "Day 6 parser" should {
     "parse line" in {
        Parser.parse(Day6.line(_))("1  2 3   4     5").get should be (Seq(
@@ -52,20 +112,19 @@ class Day6Test extends AnyWordSpec with Matchers {
          Day6.Symbol.Number(5)
        ))
     }
+    "parse to grid" in {
+      Parser.parse(Day6.grid(_))(exampleInput).get should be(exampleModel)
+    }
   }
 
   "Day 6" should {
-
-    "parse input" in {
-      Day6.parse(exampleInput) should be(exampleModel)
-    }
 
     "Part1: example answer" in {
       Day6.part1(Day6.parse(exampleInput)) should be(4277556)
     }
 
     "Part2: example answer" in {
-      Day6.part2(Day6.parse(exampleInput)) should be(0)
+      Day6.part2(Day6.parse(exampleInput)) should be(3263827)
     }
   }
 }
